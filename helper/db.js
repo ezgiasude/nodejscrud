a0837e934dd5cc04d24485d08fd5b7f28f8c8c2d
Ezgi <100479947+ezgiasude@users.noreply.github.com>
2022-09-04 17:00:16 +0300

    Update db.js
    password ***

--- helper/db.js
+++ helper/db.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 
 module.exports = () => {
-    mongoose.connect("mongodb+srv://ErdemAbii:Elma123@cluster0.2m7m54q.mongodb.net/?retryWrites=true&w=majority");
+    mongoose.connect("mongodb+srv://ErdemAbii:*****@cluster0.2m7m54q.mongodb.net/?retryWrites=true&w=majority");
 
     mongoose.connection.on('open', () => {
         console.log('MongoDB: Connected');
